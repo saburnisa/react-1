928f4026e0b82386ce9782a3a277d48fee36fd30
Rithigasiva <150768832+Rithigasiva@users.noreply.github.com>
2024-08-12 23:36:38 +0530

    Add files via upload

---
+++ day2/cw2.js
@@ -0,0 +1,10 @@
+import './cw2.css';
+const  Dis4=()=>{
+    return(
+        <div className="cw01">
+            <h1 className="green">Inline Style in JSX Example.</h1>
+            <p className="blue">This is a paragraph with inline styles applied</p>
+        </div>
+    );
+};
+export default Dis4;
